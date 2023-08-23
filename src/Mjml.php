f9baada3ad711a6547add07dfd24bbd1a7f9d3b3
Freek Van der Herten <freek@spatie.be>
2023-08-23 10:38:02 +0200

    remove call method

--- src/Mjml.php
+++ src/Mjml.php
@@ -104,17 +104,14 @@ class Mjml
         return ! $result->hasErrors();
     }
 
-    public function convert(string $mjml, array $options = []): MjmlResult
-    {
-        return $this->callMjml($mjml, $options);
-    }
+
 
     public function toHtml(string $mjml, array $options = []): string
     {
         return $this->convert($mjml, $options)->html();
     }
 
-    protected function callMjml(string $mjml, array $options = []): MjmlResult
+    public function convert(string $mjml, array $options = []): MjmlResult
     {
         $arguments = [
             $mjml,
